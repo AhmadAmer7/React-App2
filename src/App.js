046986ada3ff8f3b5459c5830e44c335d28ed0d9
Ahmadamer1 <s12011017@stu.najah.edu>
2023-05-19 09:14:56 +0200

    Fix Errors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { Component } from "react";
 import './App.css';
+import About from './components/About/About.jsx';
+import Home from './components/Home.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  render(){
+
+    return (
+    <div>
+   
+   
+     <Home/>
+     <About/>
+     
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+
